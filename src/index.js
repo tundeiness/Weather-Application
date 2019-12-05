0ee7d4213d574ebe0f0dcc0d4f6f6a49be2b0edc
tundeiness <tundeiness@yahoo.co.uk>
2019-12-05 12:53:42 +0100

    clean up index.js

--- src/index.js
+++ src/index.js
@@ -5,28 +5,9 @@ import defaultDisplay from './default';
 
 const subButton = document.getElementById('submit');
 subButton.addEventListener('click', () => {
-  // evnt.preventDefault();
-
   const cityName = document.getElementById('inputText').value.trim();
-  // console.log(cityName);
-
-  // defaultView(cityName);
   defaultDisplay(cityName);
   document.getElementById('inputText').value = ' ';
-  // document.getElementById('inline-notify').remove();
 });
 
 defaultDisplay();
-// const defaultView = defaultDisplay();
-// defaultView();
-// defaultDisplay();
-
-// console.log(cityName);
-// const view = render();
-// view();
-// const cityName = document.getElementById('inputText').value;
-// console.log(cityName);
-
-
-// const defaultView = defaultDisplay();
-// defaultView();
